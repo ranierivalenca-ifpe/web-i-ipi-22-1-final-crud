d92d7bca8cbba9dd85e497930fcf94c8df44c749
ranieri <ranierivalenca@gmail.com>
2022-12-07 15:57:52 -0300

    Adicionando estrutura básica de navegação

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,37 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Home from './lib/Home.svelte';
+  import Register from './lib/Register.svelte';
+  import Login from './lib/Login.svelte';
+  import { currentPage } from './stores';
 </script>
 
 <main>
+  <h1>My app</h1>
+  <nav>
+    <div on:click={() => $currentPage = 'home'}>Home</div>
+    <div on:click={() => $currentPage = 'register'}>Cadastrar</div>
+    <div on:click={() => $currentPage = 'login'}>Login</div>
+  </nav>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    {#if $currentPage == 'home'}
+      <Home></Home>
+    {:else if $currentPage == 'register'}
+      <Register></Register>
+    {:else if $currentPage == 'login'}
+      <Login></Login>
+    {/if}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  nav {
+    display: flex;
+    gap: 0.5em;
+    justify-content: space-between;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  nav > div {
+    border: 1px solid black;
+    padding: 0 1em;
+    cursor: pointer;
   }
 </style>
